6e00af21903f946654b22d8bd800cdaf2e5455ee
Shyamak-Seth <115006318+Shyamak-Seth@users.noreply.github.com>
2023-06-09 20:38:19 +0530

    Update index.php

--- login/index.php
+++ login/index.php
@@ -143,11 +143,4 @@
             </div>
         </div>
     </body>
-    <script>
-        const bannerElmnt = document.getElementsByTagName("div");
-        bannerElmnt[bannerElmnt.length - 1].remove();
-        const bannerScript = document.getElementsByTagName("script");
-        bannerScript[bannerScript.length - 2].remove();
-        bannerScript[bannerScript.length - 1].remove();
-    </script>
 </html>
